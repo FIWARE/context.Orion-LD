e6b7c534117fe5935676df1154a74eb0874db236
Ken Zangelin <kzangeli@gmail.com>
2024-09-03 13:48:50 +0200

    Full support for GET /info/sourceIdentity

--- src/lib/orionld/serviceRoutines/orionldGetInfo.cpp
+++ src/lib/orionld/serviceRoutines/orionldGetInfo.cpp
@@ -31,7 +31,34 @@ extern "C"
 #include "logMsg/logMsg.h"                                     // LM_*
 
 #include "orionld/common/orionldState.h"                       // orionldState
+#include "orionld/common/numberToDate.h"                       // numberToDate
+#include "orionld/serviceRoutines/orionldGetVersion.h"         // orionldGetVersion
 #include "orionld/serviceRoutines/orionldGetInfo.h"            // Own Interface
+
+
+
+// ----------------------------------------------------------------------------
+//
+// uptimeRender -
+//
+static void uptimeRender(int uptime, char* buf, int bufLen)
+{
+  int seconds = uptime % 60;
+  uptime = uptime / 60;
+
+  int minutes = uptime % 60;
+  uptime = uptime / 60;
+
+  int hours  = uptime / 24;
+  uptime = uptime / 24;
+
+  int days = uptime;
+
+  if (days > 0)
+    snprintf(buf, bufLen - 1, "P%dDT%02dH%02dM%02dS", days, hours, minutes, seconds);
+  else
+    snprintf(buf, bufLen - 1, "PT%02dH%02dM%02dS", hours, minutes, seconds);
+}
 
 
 
@@ -43,18 +70,40 @@ bool orionldGetInfo(void)
 {
   static int  runNo = 1;
   char        id[128];
+  int         uptime = orionldState.requestTime - startTime;
+  char        uptimeV[64];
+  char        currentTimeV[64];
+  KjNode*     extras;
 
+  // id
   snprintf(id, sizeof(id) - 1, "urn:ngsi-ld:cs:%06d", runNo);
   ++runNo;
 
-  KjNode* infoP  = kjObject(orionldState.kjsonP, NULL);
-  KjNode* idP    = kjString(orionldState.kjsonP, "id", id);
-  KjNode* typeP  = kjString(orionldState.kjsonP, "type", "ContextSourceIdentity");
-  KjNode* aliasP = kjString(orionldState.kjsonP, "contextSourceAlias", brokerId);
+  // uptime
+  uptimeRender(uptime, uptimeV, sizeof(uptimeV));
+
+  // current time
+  numberToDate(orionldState.requestTime, currentTimeV, sizeof(currentTimeV));
+
+  // extras
+  orionldGetVersion();
+  extras = orionldState.responseTree;
+
+  KjNode* infoP    = kjObject(orionldState.kjsonP, NULL);
+  KjNode* idP      = kjString(orionldState.kjsonP, "id", id);
+  KjNode* typeP    = kjString(orionldState.kjsonP, "type", "ContextSourceIdentity");
+  KjNode* uptimeP  = kjString(orionldState.kjsonP, "contextSourceUptime", uptimeV);
+  KjNode* currentP = kjString(orionldState.kjsonP, "contextSourceTimeAt", currentTimeV);
+  KjNode* aliasP   = kjString(orionldState.kjsonP, "contextSourceAlias", brokerId);
 
   kjChildAdd(infoP, idP);
   kjChildAdd(infoP, typeP);
+  kjChildAdd(infoP, uptimeP);
+  kjChildAdd(infoP, currentP);
   kjChildAdd(infoP, aliasP);
+  kjChildAdd(infoP, extras);
+
+  extras->name = (char*) "contextSourceExtras";
 
   orionldState.responseTree = infoP;
 
