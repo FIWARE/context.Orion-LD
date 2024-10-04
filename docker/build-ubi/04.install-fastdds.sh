777ba0ed1ff2ce4d085afa2f4301d53b9dbce212
Ken Zangelin <kzangeli@gmail.com>
2024-10-04 16:07:04 +0200

    New branch for DDS Enabler

--- docker/build-ubi/04.install-fastdds.sh
+++ docker/build-ubi/04.install-fastdds.sh
@@ -129,7 +129,8 @@ cmake --build . --target install
 cd /opt/Fast-DDS
 git clone https://github.com/eProsima/FIWARE-DDS-Enabler.git
 cd FIWARE-DDS-Enabler
-
+git fetch -a
+git co feature/typed_tests
 yum -y install lz4-devel libzstd-devel
 
 mkdir -p build/ddsenabler_participants
