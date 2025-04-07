97458826bacdffb9cc8b116d6e966c5c5226bbb2
Ken Zangelin <kzangeli@gmail.com>
2025-04-07 17:46:43 +0200

    Fixed the new functests path in compliance checks

--- scripts/check_files_compliance.py
+++ scripts/check_files_compliance.py
@@ -299,11 +299,7 @@ for root, dirs, files in os.walk(dir):
             is_orionld = True
         elif 'test/functionalTest/ftClient/' in filename:
             is_orionld = True
-        elif 'test/functionalTest/cases/0000_ngsild' in filename:
-            is_orionld = True
-        elif 'test/functionalTest/cases/0000_troe' in filename:
-            is_orionld = True
-        elif 'test/functionalTest/cases/0000_dds' in filename:
+        elif 'test/functionalTest/cases/0000_ld' in filename:
             is_orionld = True
         elif 'test/unittests/orionld' in filename:
             is_orionld = True
