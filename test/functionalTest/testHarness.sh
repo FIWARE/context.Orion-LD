6c91a3414a23506aeffc5d97c0900e8ae6a51ef7
Ken Zangelin <kzangeli@gmail.com>
2025-06-03 19:21:09 +0200

    KTrace logfile now also output on error in functests

--- test/functionalTest/testHarness.sh
+++ test/functionalTest/testHarness.sh
@@ -316,6 +316,16 @@ function exitFunction()
               # fi
           fi
 
+          if [ -s /tmp/Orion-LD.log ]
+          then
+              echo "orionld K log file"
+              echo "-------------------------------------------------"
+              cat /tmp/Orion-LD.log
+              echo "-------------------------------------------------"
+              echo
+              echo
+          fi
+
           if [ -f /tmp/accumulator_9997_stderr ]
           then
               echo "/tmp/accumulator_9997_stderr:"
