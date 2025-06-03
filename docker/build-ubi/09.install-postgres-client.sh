36f8b37440603b84f738a9953df61cca64822260
Ken Zangelin <kzangeli@gmail.com>
2025-06-03 16:09:20 +0200

    Bumping postgres client from v12 to v13 - as v12 is yum is having problems with v12 - THIS IS A TEST

--- docker/build-ubi/09.install-postgres-client.sh
+++ docker/build-ubi/09.install-postgres-client.sh
@@ -31,9 +31,9 @@ yum -y install blas libdap SuperLU lapack armadillo hdf5 xerces-c gdal-libs
 
 dnf -y module disable postgresql
 
-yum -y --nogpgcheck install postgresql12 postgresql12-contrib
+yum -y --nogpgcheck install postgresql13 postgresql13-contrib
 
 echo "---------- Install  postgres ----------"
-yum -y install libpqxx-devel postgresql12-devel postgresql12-libs
+yum -y install libpqxx-devel postgresql13-devel postgresql13-libs
 
 echo "---------- install-postgres-client.sh is DONE ----------"
