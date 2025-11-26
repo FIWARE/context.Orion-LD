7cc56eda32038ed86ff0c27f4f635705eee342a4
Ken Zangelin <kzangeli@gmail.com>
2025-11-26 21:17:24 +0100

    New version for MHD => new base image

--- docker/build-ubi/05.install-lib-microhttp.sh
+++ docker/build-ubi/05.install-lib-microhttp.sh
@@ -25,7 +25,7 @@ set -e
 
 echo
 echo -e "\e[1;32m Builder: installing libmicrohttpd \e[0m"
-curl -L http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.75.tar.gz | tar xzC ${ROOT_FOLDER}
+curl -L http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-1.0.2.tar.gz | tar xzC ${ROOT_FOLDER}
 cd ${ROOT_FOLDER}/libmicrohttpd-0.9.75
 ./configure --disable-postprocessor --disable-dauth --enable-https
 make
