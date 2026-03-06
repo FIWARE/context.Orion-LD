5eb2dd9a1f4b46d4a1599ea11f1e81f524b13087
Ken Zangelin <kzangeli@gmail.com>
2026-03-06 13:32:59 +0100

    New base image for websocks

--- docker/build-ubi/05.install-lib-microhttp.sh
+++ docker/build-ubi/05.install-lib-microhttp.sh
@@ -30,5 +30,6 @@ cd ${ROOT_FOLDER}/libmicrohttpd-1.0.2
 ./configure --disable-postprocessor --disable-dauth --enable-https
 make
 make install
+cp src/include/microhttpd_ws.h /usr/local/include/
 cd ${ROOT_FOLDER} && rm -Rf libmicrohttpd-1.0.2
 ls -l /usr/local/lib/libmicrohttpd*
