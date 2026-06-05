546b9b42c5c4781857d381525bd9372d0facc62c
Carsten Frey <frey.carsten@gmail.com>
2026-06-05 11:08:14 +0000

    Set author to Carsten Frey in new correlator files

--- src/lib/orionld/common/correlatorGet.cpp
+++ src/lib/orionld/common/correlatorGet.cpp
@@ -20,7 +20,7 @@
 * For those usages not covered by this license please contact with
 * orionld at fiware dot org
 *
-* Author: Ken Zangelin
+* Author: Carsten Frey
 */
 #include <string.h>                                            // strcspn, strncpy
 
--- src/lib/orionld/common/correlatorGet.h
+++ src/lib/orionld/common/correlatorGet.h
@@ -23,7 +23,7 @@
 * For those usages not covered by this license please contact with
 * orionld at fiware dot org
 *
-* Author: Ken Zangelin
+* Author: Carsten Frey
 */
 
 
